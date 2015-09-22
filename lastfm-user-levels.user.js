c7763d3da7a5793fffdcd1d77036c73524ff4b34
Jon <jon@powha.net>
2015-09-22 21:43:25 +0200

    fix for new version of last.fm

--- lastfm-user-levels.user.js
+++ lastfm-user-levels.user.js
@@ -2,19 +2,19 @@
 // @name           Last.fm User Levels
 // @namespace      https://github.com/Row/lastfm-userscripts
 // @description    Add Last.fm User Level ( http://stas.sh/lastfm/ ) to every last.fm profile you visit.
-// @version        0.1.2
+// @version        1.0
 // @include        http://www.last.fm/user/*
 // @include        http://www.lastfm.*/user/*
 // @include        http://cn.last.fm/user/*
 // ==/UserScript==
 
-var insertPoint = document.querySelector('div.rightCol h2.heading');
+var insertPoint = document.querySelector('.col-sidebar');
 
 if(insertPoint) {
     var imgUrl   = 'http://stas.sh/lastfm/pic.php?username=';
     var link     = document.createElement('a');
-    var userName = document.querySelector('meta[property="og:title"]').getAttribute('content');
+    var userName = document.querySelector('.avatar').getAttribute('alt');
     link.setAttribute('href', 'http://stas.sh/lastfm/');
-    link.innerHTML = '<img src="' + imgUrl + userName + '" />'
-    insertPoint.parentNode.insertBefore(link, insertPoint);
+    link.innerHTML = '<img src="' + imgUrl + userName + '" style="margin-left: -331px; position: absolute;" />'
+    insertPoint.insertBefore(link, insertPoint.firstChild);
 }
