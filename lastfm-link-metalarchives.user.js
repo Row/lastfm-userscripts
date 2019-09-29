45235038571465880728161f4eacf59d05bf3a74
Jon <jon@powha.net>
2019-09-30 00:44:31 +0200

    improved parsing of artist links when changing location.

--- lastfm-link-metalarchives.user.js
+++ lastfm-link-metalarchives.user.js
@@ -2,14 +2,13 @@
 // @name           Last.fm link to Metal Archives
 // @namespace      https://github.com/Row/lastfm-userscripts
 // @description    Creates a small M in front of each artist link on www.last.fm. The M's are linked to perform a band search on www.metal-archives.com
-// @version        2.3
+// @version        3.0
 // @include        https://www.last.fm*
 // @include        https://www.lastfm.*
 // @include        https://cn.last.fm*
 // ==/UserScript==
 
-function addStyle(css)
-{
+function addStyle(css) {
     var head, style;
     head = document.getElementsByTagName('head')[0];
     if (!head) { return; }
@@ -17,77 +16,57 @@ function addStyle(css)
     style.type = 'text/css';
     try {
         style.innerHTML = css;
-    } catch(err) {
+    } catch (err) {
         style.innerText = css;
     }
     head.appendChild(style);
 }
 
-addStyle('.LMAa { font-size: 70% !important; display: inline}' +
-         '.grid-items-item-aux-text>.LMAa, .countbar-bar-slug>.LMAa, .header-info-primary>.LMAa, .venn_title>LMAa, .chartlist-name>.LMAa, .site-links .LMAa {display:none}'
-         );
-
-function parser(doc)
-{
-    //Create a node-list of all a-tags
-    var nodeListA = doc.getElementsByTagName('a');
-
-    //Regular expression
-    //Match /music/ and one or more characters which is not /
-    var re = /\/music\/([^/#]+)$/i;
-    var id;
-
-    //Iterate through the node-list
-    for (var i = 0; i < nodeListA.length; i++) {
-        var artistLink = nodeListA[i];
-
-        if (artistLink.className.search(/(LMA)/) > -1 || artistLink.parentNode.className.search(/js-period/) > -1)
-            continue;
-
-        //Match the href against the regular expression
-        if (id = artistLink.href.match(re)) {
-
-            // Filter
-            var artist = id[1].replace(/\?.+$/, '');
-
-            //Use className as a marker
-            artistLink.className += ' LMA';
-
-            //Create the M
-            var metalLink = doc.createElement('a');
-            metalLink.href = 'http://www.metal-archives.com/search?type=band_name&searchString=' + artist;
-            metalLink.className = 'LMAa';
-            metalLink.title = 'Search ' + artist + ' on Metal Archives';
-
-            if (artistLink.className.search(/featured-track-subtitle/) > -1) {
-                metalLink.innerHTML = "Metal Archives";
-                metalLink.className += ' metadata-title';
-                artistLink.parentNode.insertBefore(metalLink, artistLink);
-            } else {
-                metalLink.innerHTML = 'M ';
-                artistLink.parentNode.insertBefore(metalLink, artistLink);
-            }
-
-            //Since the nodelist is "live".
-            i++;
-        }
+addStyle(`
+    .LMAa {
+        font-size: 70% !important;
+        display: inline;
     }
+    .grid-items-item-aux-text .LMAa, .featured-item-details .LMAa {
+        float: left;
+        margin-right: 0.5em;
+    }
+`);
 
-    //Init the ticker
-    ticker(doc,i);
+function parser() {
+    // Create a node-list of all a-tags
+    const selector = 'a'+
+                     ':not(.LMA)' +
+                     ':not(.auth-dropdown-menu-item)' +
+                     ':not([aria-hidden="true"])' +
+                     '[href^="/music"]';
+
+    const nodeListA = document.querySelectorAll(selector);
+
+    // Match /music/ and one or more characters which is not / or #
+    const re = /\/music\/([^/#]+)$/i;
+
+    nodeListA.forEach(artistLink => {
+        const match = artistLink.href.match(re);
+        if (!match) return;
+
+        // Remove uri query string if present
+        const artist = match[1].replace(/\?.+$/, '');
+
+        // Use className as a marker
+        artistLink.className += ' LMA';
+
+        // Create the M
+        const metalLink = document.createElement('a');
+        metalLink.href = 'http://www.metal-archives.com/search?type=band_name&searchString=' + artist;
+        metalLink.className = 'LMAa';
+        metalLink.title = 'Search ' + artist + ' on Metal Archives';
+        metalLink.innerHTML = 'M ';
+        artistLink.parentNode.insertBefore(metalLink, artistLink);
+    });
+
+    // Init the ticker
+    window.setTimeout(parser, 1000);
 }
 
-//Method to check if the document has changed
-function ticker(doc,aCount)
-{
-    //if there are new a-tags
-    if(aCount != doc.getElementsByTagName('a').length) {
-        parser(doc);
-        return;
-    }
-
-    //Call the ticker again
-    window.setTimeout(function() {ticker(doc,aCount);}, 1000);
-}
-
-parser(document);
+parser();
