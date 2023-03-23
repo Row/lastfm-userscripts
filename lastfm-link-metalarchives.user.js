c07385a10a9e1b3c3821bfd7854c66087f067740
Jon <jon@powha.net>
2023-03-24 00:43:16 +0100

    fix the a-tag was rendered child of the artist link instead as sibling

--- lastfm-link-metalarchives.user.js
+++ lastfm-link-metalarchives.user.js
@@ -3,7 +3,7 @@
 // @namespace      https://github.com/Row/lastfm-userscripts
 // @description    Creates a small M in front of each artist link on www.last.fm
 //                 The M's are linked to perform a band search on www.metal-archives.com
-// @version        4.0
+// @version        4.1
 // @match          https://www.last.fm/*
 // @match          https://www.lastfm.*/*
 // @match          https://cn.last.fm/*
@@ -15,7 +15,8 @@
     GM_addStyle(`
         .LMAa {
             font-size: 70% !important;
-            display: inline;
+            display: inline-block;
+            padding-right: 2px;
         }
         .grid-items-item-aux-text .LMAa, .featured-item-details .LMAa {
             float: left;
@@ -23,45 +24,46 @@
         }
     `);
     const selector = 'a:not(.auth-dropdown-menu-item):not([aria-hidden="true"])[href^="/music"]';
+
     function addMetalArchivesLink(artistLink) {
-        const artistUrl = new URL(artistLink.href);
-        const artistPath = artistUrl.pathname;
+        const artistPath = new URL(artistLink.href).pathname;
         const match = artistPath.match(/\/music\/([^/#]+)$/i);
         if (!match) return;
+
         const artistName = decodeURIComponent(match[1]);
         if (!artistName) return;
-        if (artistLink.querySelector('.LMAa')) {
-            console.log('hehehe')   
-            return; // Already processed
-    }
-        const metalLink = document.createElement('a');
-        metalLink.href = 'http://www.metal-archives.com/search?type=band_name&searchString=' + encodeURIComponent(artistName);
-        metalLink.className = 'LMAa';
-        metalLink.title = 'Search ' + artistName + ' on Metal Archives';
-        metalLink.innerText = 'M ';
-        artistLink.insertBefore(metalLink, artistLink.firstChild);
+
+        const metalLink = createMetalArchivesLink(artistName);
+        artistLink.parentNode.insertBefore(metalLink, artistLink);
     }
 
-    function handleAddedNode(node) {
-        if (node.nodeType === Node.ELEMENT_NODE && node.querySelector) {
-            for (const a of node.querySelectorAll(selector)) {
-                addMetalArchivesLink(a);
-            }
+    function createMetalArchivesLink(artistName) {
+        const metalLink = document.createElement('a');
+        metalLink.href = `https://www.metal-archives.com/search?type=band_name&searchString=${encodeURIComponent(artistName)}`;
+        metalLink.className = 'LMAa';
+        metalLink.title = `Search ${artistName} on Metal Archives`;
+        metalLink.innerText = 'M ';
+        return metalLink;
+    }
+
+    function addMetalArchivesLinks(node) {
+        const nodeListA = node.querySelectorAll(selector);
+        for (const artistLink of nodeListA) {
+            addMetalArchivesLink(artistLink);
         }
     }
+
+    addMetalArchivesLinks(document);
 
     const observer = new MutationObserver(mutations => {
         for (const mutation of mutations) {
             for (const node of mutation.addedNodes) {
-                handleAddedNode(node);
+                if (node.nodeType === Node.ELEMENT_NODE) {
+                    addMetalArchivesLinks(node);
+                }
             }
         }
     });
-
-    const nodeListA = document.querySelectorAll(selector);
-    for (const artistLink of nodeListA) {
-        addMetalArchivesLink(artistLink);
-    }
 
     observer.observe(document.body, {
         childList: true,
